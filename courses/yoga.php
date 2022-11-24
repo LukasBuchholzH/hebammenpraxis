7aba9ba26448c5a1baea6b055c0156977f044f95
David Marquant <david.marquant.93@gmail.com>
2022-11-24 20:39:26 +0100

    Add missing file

---
+++ courses/yoga.php
@@ -0,0 +1,16 @@
+<p>
+    Besonders schwangere Frauen können von Yoga profitieren. 
+    Die Kraft der Frau, neues Leben aus sich heraus zu schaffen, ist ein unglaubliches Wunder. 
+    Es ist eine Zeit des Umbruchs vom Frausein zum Muttersein und deshalb körperlich wie emotional sehr anspruchsvoll. 
+    <em>Babybauch - Yoga für Schwangere</em> fördert das Wohlbefinden ganzheitlich und kann wunderbar unterstützen, 
+    in dieser Zeit mit Veränderungen besser zurechtzukommen und dadurch die Schwangerschaft und Geburt bewusster und aktiver erleben zu können.
+</p>
+
+<p>
+    Bitte melde dich direkt über die Yogalehrerin Ivonne an: <a href="kontakt.yogatime@gmail.com">kontakt.yogatime@gmail.com</a>. 
+</p>
+
+<p>
+    Ivonne ist 500h+ zertifizierte Yogalehrerin und der Kurs wurde nach § 20 Abs. 1 SGB V von der Zentrale Prüfstelle Prävention geprüft.
+    Je nach Krankenkasse kannst Du Dir 80% - 100% der Kursgebühr von deiner gesetzlichen Krankenkasse erstatten lassen. 
+</p>
