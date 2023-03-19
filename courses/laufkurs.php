cba07f46e8543d044f75ad82345881653f65cdb7
David Marquant <david.marquant.93@gmail.com>
2023-03-19 19:31:43 +0100

    File

---
+++ courses/laufkurs.php
@@ -0,0 +1,15 @@
+<p>
+    Dieser Kurs richtet sich an alle Mütter, die ihren Rückbildungskurs erfolgreich abgeschlossen haben und 
+    jetzt unter Anleitung wieder oder ganz neu in den Laufsport einsteigen möchten.
+</p>
+
+<p>Am Ende des Kurses läufst du ohne Probleme 30 Minuten bzw. fünf Kilometer am Stück.</p>
+
+<p>
+    Weitere Inhalte des Kurses:
+    <ul>
+        <li>Aufwärmübungen</li>
+        <li>Lauf-ABC zur Verbesserung des Laufstils</li>
+        <li>Dehnübungen</li>
+    </ul>
+</p>
