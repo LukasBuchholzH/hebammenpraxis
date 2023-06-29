3b9af257488100bed998aee305638870f4f035d4
David Marquant <david.marquant.93@gmail.com>
2023-06-29 18:14:57 +0200

    FIx deploy

--- deploy.bat
+++ deploy.bat
@@ -6,6 +6,7 @@ php\php.exe -c php\php.ini-development index.php > deploy\index.html
 php\php.exe -c php\php.ini-development courses.php > deploy\courses.html
 php\php.exe -c php\php.ini-development leistungen.php > deploy\leistungen.html
 php\php.exe -c php\php.ini-development rooms.php > deploy\rooms.html
+php\php.exe -c php\php.ini-development midwives.php > deploy\midwives.html
 
 echo F | xcopy /Y impressum.html deploy\impressum.html
 xcopy /Y /S css deploy\css\
