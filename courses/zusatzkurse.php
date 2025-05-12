6622e6a3142d9ff21dce22b6a95894bf69c6c25f
LukasBuchholzH <82599275+LukasBuchholzH@users.noreply.github.com>
2025-05-12 20:46:43 +0200

    Update zusatzkurse.php

--- courses/zusatzkurse.php
+++ courses/zusatzkurse.php
@@ -1,5 +1,5 @@
-<p>Kurstermine: 01.07/08.07./15.07./22.07./29.07. und 05.08.</p>
-<p>Uhrzeit:jeweils von 18:30 Uhr bis 20:30 Uhr </p>
-<p>Partner Gebühr: 110 Euro pro Partner dabei ab 15. Juli.</p>
-<p>Kursleitung:Frederica Pischedda</p>
+<p>Kurstermine:     01.07/08.07./15.07./22.07./29.07. und 05.08.</p>
+<p>Uhrzeit:        jeweils von 18:30 Uhr bis 20:30 Uhr </p>
+<p>Partner Gebühr: 110 Euro pro Partner dabei ab 15.07.</p>
+<p>Kursleitung:    Frederica Pischedda</p>
 <p> Anmeldung über <a href="mailto:hebamme.federicap@gmail.com">hebamme.federicap@gmail.com</a></p>
