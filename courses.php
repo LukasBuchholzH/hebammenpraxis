6ae31f134b44485067404ae9b38851f33974e452
LukasBuchholzH <82599275+LukasBuchholzH@users.noreply.github.com>
2025-05-12 18:42:24 +0200

    Update courses.php

--- courses.php
+++ courses.php
@@ -17,7 +17,7 @@ function initCourse($title, $keyword) {
 $courselist = array(
   "rueckbildung" => initCourse('Rückbildungskurse', 'Rückbildung'),
   "geburtsvorbereitung" => initCourse('Geburtsvorbereitung', 'Geburtsvorbereitung'),
-  "weitgeburtsvorbereitung" => initCourse('Weitere Geburtsvorbereitungskurse', 'Geburtsvorbereitung'),
+  "extrageburtsvorbereitung" => initCourse('Weitere Geburtsvorbereitungskurse', ''),
   "babymassagepapa" => initCourse('Babymassage - Papaedition', 'Babymassage - Papaedition'),
   "babymassage"  => initCourse('Babymasage', 'Babymassage'),
   "akupunktur" => initCourse('Akupunktur', 'Akupunktur'),
