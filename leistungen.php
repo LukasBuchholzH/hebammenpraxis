de1b6d12a12f6bb694e11586572fa046789e5ec7
LukasBuchholzH <82599275+LukasBuchholzH@users.noreply.github.com>
2025-05-12 17:18:15 +0200

    Update leistungen.php
    Text Beckenboden

--- leistungen.php
+++ leistungen.php
@@ -301,11 +301,26 @@ include 'templates/head.php';
 
     <h3 id="leistung-beckenbodensprech">Beckenbodensprechstunde</h3>
     <p>
-      
+      Der Beckenboden und besonders seine Schwächen wurden viel zu lange mit Scham behandelt oder gar ganz verschwiegen. 
+      Nun bekommt dieser zum Glück immer mehr Aufmerksamkeit; dank der aktiven Hebammenarbeit und auch von der Arbeit einiger FrauenärztInnen und UrologInnen.   
     </p>
 
     <p>
-      
+      Mein Name ist Stella Troilo, ich bin Fachärztin für Gynäkologie und Geburtshilfe und in einer Praxis tätig.
+      In der Klinik lernt man wenig über die konservativen (nicht-operativen) Behandlungsmöglichkeiten, so dass ich mich in meiner Praxislaufbahn aktiv durch Fortbildungen, Kongresse und Workshops immer mehr in die konservative Therapie der Beckenbodenschwäche eingearbeitet habe.
+      Da der normale Praxisalltag kaum Zeit für dieses Anliegen gibt, biete ich eine Beckenbodensprechstunde an. 
+      Mir liegt am Herzen, dass du durch eine ausführliche Beratung und Untersuchung Therapiemöglichkeiten und Verständnis für deinen individuellen Beckenboden bekommst. 
+      Was kannst du verbessern? Welche Hilfsmittel/Physiotherapie/Trainingsgeräte sind passend? 
+      Und was wird dein Beckenboden vielleicht auch nicht mehr leisten können? 
+      In dieser Sprechstunde schauen wir ganz individuell, wie du deine Lebensqualität verbessern kannst.  
+    </p>
+
+    <p>
+      Kontakt: <a href="mailto:beckenboden.mannheim@gmail.com">beckenboden.mannheim@gmail.com</a> 
+
+      Per Whatsapp +49 15560 951484 
+
+      Mehr Infos findest du auf meiner Homepage: <a href="www.stressreliefmannheim.de">www.stressreliefmannheim.de</a>
     </p>
     
   </section>
