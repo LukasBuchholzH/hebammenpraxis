d821b048e5106a9a45637a841cbd965460c259f9
LukasBuchholzH <82599275+LukasBuchholzH@users.noreply.github.com>
2025-08-21 17:18:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -62,13 +62,6 @@ include 'templates/head.php';
     <p>
        Kontoverbindung für die Betreuungspauschale : IBAN: DE94 6704 0031 0710 5042 00
     </p>
-  <h3>Weiterer Geburtsvorbereitungskurs 1.7.2025 - 5.8.2025</h3> 
-    <p>Kurstermine:     01.07/08.07./15.07./22.07./29.07. und 05.08.</p>
-    <p>Uhrzeit:        jeweils von 18:30 Uhr bis 20:30 Uhr </p>
-    <p>Partner Gebühr: 110 Euro pro Partner dabei ab 15.07.</p>
-    <p>Kursleitung:    Federica Pischedda</p>
-    <div style="margin-right: 12px; margin-top: 32px;"><a href="https://pischedda.hebamio.de/anmeldung?reason=course-1" class="btn">Zur Anmeldung</a></div>
-
   </section>
 
   <section id="contact">
@@ -98,3 +91,4 @@ include 'templates/head.php';
 
 </body>
 </html>
+
