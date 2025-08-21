d8cda9445e738babfe2d4f6f8c5922ed16a4841a
LukasBuchholzH <82599275+LukasBuchholzH@users.noreply.github.com>
2025-08-21 18:47:54 +0200

    Update index.php

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ include 'templates/head.php';
   <section id="news">
     <h2>Aktuelles aus der Praxis</h2>
     
-<p>Neue Sportkurse mit und ohne Baby online. Es gibt noch freie Plätze. Zur Anmeldung <a href="courses<?= $suffix ?>#mamaworkout">"Hier"</a></p>
+<p>Neue Sportkurse mit und ohne Baby online. Es gibt noch freie Plätze. Zur Anmeldung geht es<a href="courses<?= $suffix ?>#mamaworkout">hier</a>.</p>
     
     <h3>Betreuungspauschale</h3>
     <p>
@@ -93,5 +93,6 @@ include 'templates/head.php';
 
 </body>
 </html>
+
 
 
