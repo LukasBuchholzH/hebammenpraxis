c339a06d25bb7d3b3b83deaf58f47fe5a89de570
LukasBuchholzH <82599275+LukasBuchholzH@users.noreply.github.com>
2025-08-21 17:17:21 +0200

    Delete courses/zusatzkurse.php

--- courses/zusatzkurse.php
+++
@@ -1,6 +0,0 @@
-<p>Geburtsvorbereitungskurs 1.7.2025 - 5.8.2025</p> 
-<p>Kurstermine:     01.07/08.07./15.07./22.07./29.07. und 05.08.</p>
-<p>Uhrzeit:        jeweils von 18:30 Uhr bis 20:30 Uhr </p>
-<p>Partner Gebühr: 110 Euro pro Partner dabei ab 15.07.</p>
-<p>Kursleitung:    Federica Pischedda</p>
-<div style="margin-right: 12px; margin-top: 32px;"><a href="https://pischedda.hebamio.de/anmeldung?reason=course-1" class="btn">Zur Anmeldung</a></div>
