d7aa3634c6cbab608fe8c2bd87547f13a71ce323
LukasBuchholzH <82599275+LukasBuchholzH@users.noreply.github.com>
2025-11-07 10:54:35 +0100

    Create beckenboden.php

---
+++ courses/beckenboden.php
@@ -0,0 +1,9 @@
+<p>"Nach der Geburt verdient dein Körper besondere Aufmerksamkeit und Fürsorge. Das „Leben bewegt“-Konzept bietet dir ein modernes, effektives und wissenschaftlich fundiertes Bauch- und Beckenbodentraining, das speziell auf die Bedürfnisse von Frauen abgestimmt ist.</p>
+
+<p>Du lernst, deinen Beckenboden bewusst wahrzunehmen und im Zusammenspiel mit der Atmung, dem Becken, dem Rumpf und den Beinen zu trainieren. Die Übungen werden dir dabei helfen, dein Körpergefühl zu verbessern. So wirst du in deinem Alltag und bei körperlichen Aktivitäten sicherer und beweglicher.</p>
+
+<p>Neben der funktionellen Kräftigung deines gesamten Körpers, steht die Entspannung und Regeneration ebenso im Fokus wie achtsame Körperwahrnehmung und gesunde Haltung. Wir unterstützen dich dabei, dich in deinem Körper wieder wohl und stark zu fühlen und nehmen Rücksicht auf deine individuelle Situation und Bedürfnisse.</p>
+
+<p>Der Kurs ist für alle Frauen geeignet die gezielt ihre Mitte stärken wollen und „dran bleiben“ möchten.</p>
+
+<p><strong>Dieser Kurs ist von der Zentrale Prüfstelle Prävention gemäß §20,1 SGBV zertifiziert (Kräftigung Beckenboden) und deshalb von allen gesetzlichen Krankenkassen bezuschussungsfähig".</strong></p>
