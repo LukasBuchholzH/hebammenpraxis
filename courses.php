174ee6b3dd9aae5b75d9d509797efad629f40c60
LukasBuchholzH <82599275+LukasBuchholzH@users.noreply.github.com>
2025-11-07 11:35:35 +0100

    Update courses.php

--- courses.php
+++ courses.php
@@ -15,7 +15,7 @@ function initCourse($title, $keyword) {
 }
 
 $courselist = array(
-  "Beckenbodenpräventionskurs" => initCourse('Beckenbodenpräventionskurs', 'Beckenboden'),
+  "Beckenbodenpräventionskurs" => initCourse('Beckenboden', 'Beckenbodenpräventionskurs'),
   "rueckbildung" => initCourse('Rückbildungskurse', 'Rückbildung'),
   "geburtsvorbereitung" => initCourse('Geburtsvorbereitung', 'Geburtsvorbereitung'),  
   "babymassage"  => initCourse('Babymassage', 'Babymassage'),
@@ -204,6 +204,7 @@ include 'templates/head.php';
 
 </body>
 </html>
+
 
 
 
