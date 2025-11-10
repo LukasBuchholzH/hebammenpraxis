40f6e91fe31e9885a5d8e7c241d4242d5fff9681
LukasBuchholzH <82599275+LukasBuchholzH@users.noreply.github.com>
2025-11-10 10:46:53 +0100

    Update rueckbildung.php

--- courses/rueckbildung.php
+++ courses/rueckbildung.php
@@ -1,35 +1,5 @@
-<p>
-Rückbildungsgymnastik in Form von Beratung und praktischen Übungen Informationen über körperliche
-Veränderung und Maßnahmen nach Schwangerschaft, Geburt und Wochenbett Erklärungen zur Funktion des 
-Beckenbodens und Erläuterung der Lage innerer Organe Wahrnehmung, Kontrolle und Kräftigung des Beckenbodens 
-(u.a. Senkungs-, Inkontinenzprophylaxe) allgemeine Kräftigung des Bewegungs- und Halteapparates durch Übungen 
-(Information und praktische Anleitung zum Alltag mit Baby, z.B. Heben, Tragen und Stehen - Ergonomie) 
-Venentraining Körperarbeit, z.B. Entspannungsübungen
-</p>
+<p>Ab 01.01.2026 bieten wir auf Grund des zum 01.11.2025 Inkraftgetretenem Hebammenhilfevertrages keine Rückbildungskurse mehr in unserer Praxis an.</p>
 
-<strong>Rückbildungskurs Vormittags:</strong>
+<p>Durch die neuen Abrechnungsregelungen bei Kursen ist es uns nicht mehr möglich diese kostendeckend durchzuführen.</p>
 
-<p>Das Baby darf gerne dabei sein, ist aber kein Muss.</p>
-
-<strong>Rückbildungskurs Abends:</strong>
-
-<p>In dieser Zeit kannst du dich ohne dein Kind voll auf die Übungen konzentrieren.</p>
-
-<strong>Rückbildung Crashkurs:</strong>
-
-<p>Die Rückbildung im Crashkurs-Format umfasst insgesamt 8 Termine und bietet die gleichen Inhalte wie unser regulärer Kurs. Der Unterschied besteht darin, dass die Termine zweimal pro Woche stattfinden, sodass der Kurs in nur 4 Wochen abgeschlossen ist.</p>
-<p>Die Babys sind beim Crashkurs willkommen.</p>
-
-<strong>Pränatales Empowerment mit hypnotischer Tiefenentspannung:</strong>
-
-<p>
-Du hast einen Geburtsvorbereitungskurs besucht und möchtest diesen durch gezielte Übungen und Entspannungstechniken ergänzen?
-</p>
-
-<p>
-In diesem Workshop erlernst oder vertiefst du die Wahrnehmung deines Beckenbodens in Verbindung mit deiner Atmung und stärkst deine Körpermitte. Am Ende erwartet dich eine ausführliche Entspannung mit Hypnoseelementen.
-</p>
-</br>
-<strong>Hinweis:</strong>
-<p>Die Anmeldung zu den Rückbildungskursen ist <strong>ausschließlich</strong> nach der Geburt möglich.</br> Anmeldungen vor der Geburt können leider nicht berücksichtigt werden.</br> Wir bitten um Verständnis.</p>
-
+<p>Wir bitten um Verständnis,  andernfalls dürfen Sie sich gerne an den <a href="https://gkv-bund.de/" target="_blank">GKV-Spitzenverband</a> wenden und sich damit politisch für Frauen- und Familiengesundheit engagieren.</p>
