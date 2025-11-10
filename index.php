3cc0e80f346923e32130d6ddf9b311a1bbd7d6ba
LukasBuchholzH <82599275+LukasBuchholzH@users.noreply.github.com>
2025-11-10 10:41:58 +0100

    Update index.php
    Beckenbodenkurs

--- index.php
+++ index.php
@@ -44,6 +44,7 @@ include 'templates/head.php';
     <h2>Aktuelles aus der Praxis</h2>
     
 <p>Ab sofort bieten wir als eine weitere Leistung eine <a href="leistungen<?= $suffix ?>#leistung-sprechstunde">"offene Sprechstunde"</a> an.</p>
+<p>Ebenfalls bieten wir ab sofort nun <a href="courses<?= $suffix ?>#beckenboden">Beckenbodenpräventionskurse</a> an.</p>   
     
     <h3>Betreuungspauschale</h3>
     <p>
@@ -93,6 +94,7 @@ include 'templates/head.php';
 
 </body>
 </html>
+
 
 
 
