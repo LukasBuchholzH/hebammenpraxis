ebf9b269d61c82e273647a57f60684aafdadcf8a
LukasBuchholzH <82599275+LukasBuchholzH@users.noreply.github.com>
2025-11-13 11:38:17 +0100

    Update beckenboden.php

--- courses/beckenboden.php
+++ courses/beckenboden.php
@@ -1,6 +1,6 @@
 <p>In bestimmten Lebensphasen verdient dein Körper besondere Aufmerksamkeit und Fürsorge. Das „Leben bewegt“-Konzept bietet dir ein modernes, effektives und wissenschaftlich fundiertes Bauch- und Beckenbodentraining, das speziell auf die Bedürfnisse von Frauen abgestimmt ist.</p>
 
-<p>DDu lernst, deinen Beckenboden bewusst wahrzunehmen und im Zusammenspiel mit der Atmung, dem Becken, dem Rumpf und den Beinen zu trainieren. Die Übungen werden dir dabei helfen, dein Körpergefühl zu verbessern. So wirst du in deinem Alltag und bei körperlichen Aktivitäten sicherer und beweglicher.</p>
+<p>Du lernst, deinen Beckenboden bewusst wahrzunehmen und im Zusammenspiel mit der Atmung, dem Becken, dem Rumpf und den Beinen zu trainieren. Die Übungen werden dir dabei helfen, dein Körpergefühl zu verbessern. So wirst du in deinem Alltag und bei körperlichen Aktivitäten sicherer und beweglicher.</p>
 
 <p>Neben der funktionellen Kräftigung deines gesamten Körpers, steht die Entspannung und Regeneration ebenso im Fokus wie achtsame Körperwahrnehmung und gesunde Haltung. Wir unterstützen dich dabei, dich in deinem Körper wieder wohl und stark zu fühlen und nehmen Rücksicht auf deine individuelle Situation und Bedürfnisse.</p>
 
