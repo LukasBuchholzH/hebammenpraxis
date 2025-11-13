f9a87dde3e5957da28a72a5d15c288b4f58d33a9
LukasBuchholzH <82599275+LukasBuchholzH@users.noreply.github.com>
2025-11-13 11:04:18 +0100

    Update rueckbildung.php

--- courses/rueckbildung.php
+++ courses/rueckbildung.php
@@ -1,5 +1,6 @@
-<p>Ab 01.01.2026 bieten wir auf Grund des zum 01.11.2025 Inkraftgetretenem Hebammenhilfevertrages keine Rückbildungskurse mehr in unserer Praxis an.</p>
+<p>Ab 01.01.2026 bieten wir auf Grund des zum 01.11.2025 inkraftgetretenen Hebammenhilfevertrages keine Rückbildungskurse mehr in unserer Praxis an.</p>
 
 <p>Durch die neuen Abrechnungsregelungen bei Kursen ist es uns nicht mehr möglich diese kostendeckend durchzuführen.</p>
 
 <p>Wir bitten um Verständnis,  andernfalls dürfen Sie sich gerne an den <a href="https://gkv-bund.de/" target="_blank">GKV-Spitzenverband</a> wenden und sich damit politisch für Frauen- und Familiengesundheit engagieren.</p>
+
