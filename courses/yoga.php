d05dc06199a155200e3dcc9bbe7344ba4764a0ea
LukasBuchholzH <82599275+LukasBuchholzH@users.noreply.github.com>
2025-12-03 17:04:02 +0100

    Update yoga.php

--- courses/yoga.php
+++ courses/yoga.php
@@ -2,7 +2,7 @@
     Besonders schwangere Frauen können von Yoga profitieren. 
     Die Kraft der Frau, neues Leben aus sich heraus zu schaffen, ist ein unglaubliches Wunder. 
     Es ist eine Zeit des Umbruchs vom Frausein zum Muttersein und deshalb körperlich wie emotional sehr anspruchsvoll. 
-    <em>Hatha Yoga Flow für Schwangere</em> fördert das Wohlbefinden ganzheitlich und kann wunderbar unterstützen, 
+    <em>Yoga Flow für Schwangere</em> fördert das Wohlbefinden ganzheitlich und kann wunderbar unterstützen, 
     in dieser Zeit mit Veränderungen besser zurechtzukommen und dadurch die Schwangerschaft und Geburt bewusster und aktiver erleben zu können.
 </p>
 
@@ -14,6 +14,7 @@
 
 <div>Kursleitung: Ivonne Scharff</div>
 <div>Kursort: Hebammenpraxis Oststadt</div>
-<div>Kusrkosten: 135€ - Erstattungsfähig über die Krankenkassen</div>
+<div>Kusrkosten: 140€. Die Erstattung über die Krankenkasse erfolgt nach Kursende. </div>
 
-<div style="margin-right: 12px; margin-top: 32px;"><a href="https://sites.google.com/view/yogatimemannheim/kurstermine" class="btn">Zur Anmeldung</a></div>
+<div style="margin-right: 12px; margin-top: 32px;"><a href="www.momoyoga.com/mahila/register" class="btn">Zur Anmeldung</a></div>
+<p>Um Deinen Platz zu reservieren, wähle nach der Registrierung auf momoyoga deinen Event aus und klicke auf "hol dir ein Ticket". Eine Stornierung und Rückerstattung des gezahlten Tickets ist selbstverständlich möglich.</p>
