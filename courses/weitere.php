793da2a41fb8796da03bc5c64aaca15a98a0df9d
LukasBuchholzH <82599275+LukasBuchholzH@users.noreply.github.com>
2025-12-03 17:46:04 +0100

    Update weitere.php

--- courses/weitere.php
+++ courses/weitere.php
@@ -1,3 +1,45 @@
+<h2>Yoga Flow für Schwangere</h2>
+<p>
+    Besonders schwangere Frauen können von Yoga profitieren. 
+    Die Kraft der Frau, neues Leben aus sich heraus zu schaffen, ist ein unglaubliches Wunder. 
+    Es ist eine Zeit des Umbruchs vom Frausein zum Muttersein und deshalb körperlich wie emotional sehr anspruchsvoll. 
+    <em>Yoga Flow für Schwangere</em> fördert das Wohlbefinden ganzheitlich und kann wunderbar unterstützen, 
+    in dieser Zeit mit Veränderungen besser zurechtzukommen und dadurch die Schwangerschaft und Geburt bewusster und aktiver erleben zu können.
+</p>
+
+<p>
+    Ivonne ist 500h+ zertifizierte Yogalehrerin und der Kurs wurde nach § 20 Abs. 1 SGB V von der Zentrale Prüfstelle Prävention geprüft.
+    Je nach Krankenkasse kannst du dir 80% - 100% der Kursgebühr von deiner gesetzlichen Krankenkasse erstatten lassen. 
+</p>
+
+
+<div>Kursleitung: Ivonne Scharff</div>
+<div>Kursort: Hebammenpraxis Oststadt</div>
+<div>Kusrkosten: 140€. Die Erstattung über die Krankenkasse erfolgt nach Kursende. </div>
+
+<div style="margin-right: 12px; margin-top: 32px;"><a href="www.momoyoga.com/mahila/register" class="btn">Zur Anmeldung</a></div>
+<p>Um Deinen Platz zu reservieren, wähle nach der Registrierung auf momoyoga deinen Event aus und klicke auf "hol dir ein Ticket". Eine Stornierung und Rückerstattung des gezahlten Tickets ist selbstverständlich möglich.</p>
+
+
+<h2>Yoga Flow mit Baby möglich</h2>
+<p>
+    Du wirst kraft und Erholung tanken. 
+    Die Kleinen können bei euch liegen, mitmachen, spielen oder schlafen.
+    Dein Baby sollte noch nicht laufen können.
+    Wenn du magst, kannst du dich mit anderen Mamas vernetzen.
+    Ein zuvor abgeschlossener Rückbildungskurs ist von Vorteil.
+</p>
+
+<div>Kursleitung: Ivonne Scharff</div>
+<div>Kursort: Hebammenpraxis Oststadt</div>
+<div>Kusrkosten: 160€ - Die Erstattung über die Krankenkasse erfolgt nach Kursende. </div>
+
+<div style="margin-right: 12px; margin-top: 32px;"><a href="www.momoyoga.com/mahila/register" class="btn">Zur Anmeldung</a></div>
+<p>Um Deinen Platz zu reservieren, wähle nach der Registrierung auf momoyoga deinen Event aus und klicke auf "hol dir ein Ticket". Eine Stornierung und Rückerstattung des gezahlten Tickets ist selbstverständlich möglich.</p>
+
+
+
+
 <h2>Trage-Workshop</h2>
 <p>
 "Wie trage ich mein Baby richtig?" Workshop für Schwangere und werdende Eltern Ihr wollt schon vor der Geburt eures Babys gut vorbereitet sein, um es direkt sicher tragen zu können? Im Trageworkshop in der Hebammenpraxis erfahrt ihr als werdende Eltern alles übers
@@ -13,3 +55,4 @@ Mein Name ist Lara Holldorf, ich bin zertifizierte Trageberaterin (Fachkraft fü
 <div>Kurskosten: 40€ pro Familie</div>
 
 <div style="margin-right: 12px; margin-top: 32px;"><a href="https://trageberatung-weinheim.de/" class="btn">Zur Anmeldung</a></div>
+
