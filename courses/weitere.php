39cb3492b666f9b4484765632923e17031aeeeaf
LukasBuchholzH <82599275+LukasBuchholzH@users.noreply.github.com>
2025-12-03 18:09:15 +0100

    Update weitere.php

--- courses/weitere.php
+++ courses/weitere.php
@@ -1,3 +1,51 @@
+<h2>Baby- und Kindernotfallkurs</h2>
+<p>
+Babys und Kleinkinder haben noch kein Gefahrenbewusstsein. 
+Jeder Tag ist ein Abenteuer. 
+Es gibt so viel zu entdecken und zu lernen. 
+Und natürlich passieren dabei auch kleinere und größere Unfälle. 
+Welche Unfälle du verhindern kannst, und wie du hilfst, wenn es doch passiert ist, das lernst du in diesem Kurs. 
+</p>
+
+<p>
+In 3 Stunden besprechen wir in kleiner Runde die häufigsten Notfälle im Säuglings- und Kleinkindalter, sodass du nach dem Kurs das Gefühl hast, mit Sicherheit helfen zu können. 
+Es können gerne auch Oma, Opa und der Babysitter teilnehmen. 
+Denn im Notfall sollte jeder wissen, was zu tun ist.
+</p>
+
+<p>
+  Die Themen des Kurses sind: 
+  <ul>
+    <li>Fieberkrampf</li>
+    <li>Pseudokrupp</li>
+    <li>Bewusstlosigkeit</li> 
+    <li>Stabile Seitenlage</li>
+    <li>Wiederbelebung</li>
+    <li>Plötzlicher Kindstod</li>
+    <li>Verschluckter Fremdkörper</li>
+    <li>Vergiftung</li>
+    <li>Kopfverletzungen</li>
+    <li>Verbrennungen</li>
+    <li>Insektenstiche</li>
+    <li>Ertrinken</li>
+    <li>Hausapotheke für Kinder</li>
+  </ul>
+</p>
+
+<p>
+Selbstverständlich üben wir an Simulationspuppen. 
+Ein ausführliches Handout erhaltet ihr nach dem Kurs. 
+Im Onlinekurs mache ich die Übungen sehr detailgetreu vor, der Inhalt ist ansonsten der Gleiche. 
+</p>
+
+<p>Kurskosten: 55€ pro Person und 100€ für Paare</p>
+
+<p>Kursleitung: Anja Jaburek, Kinderkrankenschwester an der Universitätsmedizin Mannheim, Erste Hilfe Ausbilderin, BG zertifiziert.</p>
+
+<div style="margin-right: 12px; margin-top: 32px;"><a href="https://anjajaburek.de" class="btn">Zur Anmeldung</a></div>
+
+
+
 <h2>Yoga Flow für Schwangere</h2>
 <p>
     Besonders schwangere Frauen können von Yoga profitieren. 
@@ -39,7 +87,6 @@
 
 
 
-
 <h2>Trage-Workshop</h2>
 <p>
 "Wie trage ich mein Baby richtig?" Workshop für Schwangere und werdende Eltern Ihr wollt schon vor der Geburt eures Babys gut vorbereitet sein, um es direkt sicher tragen zu können? Im Trageworkshop in der Hebammenpraxis erfahrt ihr als werdende Eltern alles übers
@@ -55,5 +102,6 @@ Mein Name ist Lara Holldorf, ich bin zertifizierte Trageberaterin (Fachkraft fü
 <div>Kurskosten: 40€ pro Familie</div>
 
 <div style="margin-right: 12px; margin-top: 32px;"><a href="https://trageberatung-weinheim.de/" class="btn">Zur Anmeldung</a></div>
+
 
 
