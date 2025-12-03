d3c777af39bf62d13815e2665e86fdbaa7016804
LukasBuchholzH <82599275+LukasBuchholzH@users.noreply.github.com>
2025-12-03 18:10:46 +0100

    Update yoga.php

--- courses/yoga.php
+++ courses/yoga.php
@@ -16,5 +16,5 @@
 <div>Kursort: Hebammenpraxis Oststadt</div>
 <div>Kusrkosten: 140€. Die Erstattung über die Krankenkasse erfolgt nach Kursende. </div>
 
-<div style="margin-right: 12px; margin-top: 32px;"><a href="www.momoyoga.com/mahila/register" class="btn">Zur Anmeldung</a></div>
+<div style="margin-right: 12px; margin-top: 32px;"><a href="https://www.momoyoga.com/mahila/register" class="btn">Zur Anmeldung</a></div>
 <p>Um Deinen Platz zu reservieren, wähle nach der Registrierung auf momoyoga deinen Event aus und klicke auf "hol dir ein Ticket". Eine Stornierung und Rückerstattung des gezahlten Tickets ist selbstverständlich möglich.</p>
