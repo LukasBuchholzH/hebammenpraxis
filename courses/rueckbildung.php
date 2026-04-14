45073bbad842716dce77cb8991348eb587effb48
LukasBuchholzH <82599275+LukasBuchholzH@users.noreply.github.com>
2026-04-14 23:59:33 +0200

    Update rueckbildung.php

--- courses/rueckbildung.php
+++ courses/rueckbildung.php
@@ -18,17 +18,8 @@ Venentraining Körperarbeit, z.B. Entspannungsübungen
 <strong>Rückbildung Crashkurs:</strong>
 
 <p>Die Rückbildung im Crashkurs-Format umfasst insgesamt 8 Termine und bietet die gleichen Inhalte wie unser regulärer Kurs. Der Unterschied besteht darin, dass die Termine zweimal pro Woche stattfinden, sodass der Kurs in nur 4 Wochen abgeschlossen ist.</p>
-<p>Die Babys sind beim Crashkurs willkommen.</p>
+<p>Die Babys sind beim Crashkurs am Vormittag willkommen.</p>
 
-<strong>Pränatales Empowerment mit hypnotischer Tiefenentspannung:</strong>
-
-<p>
-Du hast einen Geburtsvorbereitungskurs besucht und möchtest diesen durch gezielte Übungen und Entspannungstechniken ergänzen?
-</p>
-
-<p>
-In diesem Workshop erlernst oder vertiefst du die Wahrnehmung deines Beckenbodens in Verbindung mit deiner Atmung und stärkst deine Körpermitte. Am Ende erwartet dich eine ausführliche Entspannung mit Hypnoseelementen.
-</p>
 </br>
 <strong>Hinweis:</strong>
 <p>Die Anmeldung zu den Rückbildungskursen ist <strong>ausschließlich</strong> nach der Geburt möglich.</br> Anmeldungen vor der Geburt können leider nicht berücksichtigt werden.</br> Wir bitten um Verständnis.</p>
